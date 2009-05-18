0c7862cca10108ffecb63e08c532df4c571eec70
oleg.ilyenko <oleg.ilyenko@4dbd696c-0aa8-11de-b55b-d397e7115e68>
2009-05-18 19:09:27 +0000

    test

--- flex-compile/flex-compile-server/src/main/java/org/oleg/fcs/project/model/Project.java
+++ flex-compile/flex-compile-server/src/main/java/org/oleg/fcs/project/model/Project.java
@@ -16,6 +16,12 @@ public class Project {
 
     private Date lastModificationDate;
 
+    private List<String> defaultLocales;
+
+    private String defaultFlexConfig;
+
+    private List<Theme> defaultThemes;
+
     public Date getLastModificationDate() {
         return lastModificationDate;
     }
@@ -43,5 +49,29 @@ public class Project {
     @Override
     public String toString() {
         return "Project '" + name + "'";
+    }
+
+    public List<String> getDefaultLocales() {
+        return defaultLocales;
+    }
+
+    public void setDefaultLocales(List<String> defaultLocales) {
+        this.defaultLocales = defaultLocales;
+    }
+
+    public String getDefaultFlexConfig() {
+        return defaultFlexConfig;
+    }
+
+    public void setDefaultFlexConfig(String defaultFlexConfig) {
+        this.defaultFlexConfig = defaultFlexConfig;
+    }
+
+    public List<Theme> getDefaultThemes() {
+        return defaultThemes;
+    }
+
+    public void setDefaultThemes(List<Theme> defaultThemes) {
+        this.defaultThemes = defaultThemes;
     }
 }
